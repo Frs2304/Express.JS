39672cc77e0eef149c967cc2c682bb1584b3af3b
tusharmishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-01-30 21:08:46 +0530

    Initial

---
+++ data/blogs.js
@@ -0,0 +1,26 @@
+{
+    blogs = [
+        {
+            title: "How to get started with JavaScript",
+            content: "This is JavaScript content",
+            slug: "learn-javascript"
+        },
+        {
+            title: "How to get started with Python",
+            content: "This is Python content",
+            slug: "learn-python"
+        },
+        {
+            title: "How to get stqrted with Node.js",
+            content: "This is Node.js content",
+            slug: "learn-node.js"
+        },
+        {
+            title: "How to get started with Docker",
+            content: "This is Docker content",
+            slug: "learn-docker"
+
+        },
+    ]
+}
+module.exports = blogs;
