cf3f46237f40a652b0acd4e6b295d02baa28f09e
Tushar Mishra <tusharmishra2304@gmail.com>
2023-05-12 23:54:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname,"static")))
-app.use('/',require(path.join(__dirname,'routes/blog.js')))
+app.use(express.static(path.join(__dirname,"static")));
+app.use('/',require(path.join(__dirname,'routes/blog.js')));
 app.listen(port,()=>{
-    console.log(`Example app is listening at http://localhost:${port}`)
-})
+    console.log(`Example app is listening at http://localhost:${port}`);
+});
